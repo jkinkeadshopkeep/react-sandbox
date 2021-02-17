8467b0e7bb0a99616941bbdbe2b96c7640a827f4
Joshua Kinkead <jkinkead@shopkeep.com>
2021-02-17 16:31:03 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Form from './components/form/form';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Form />
   </React.StrictMode>,
   document.getElementById('root')
 );
