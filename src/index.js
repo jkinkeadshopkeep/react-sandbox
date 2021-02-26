166b492a50a0978bf35542349c300f78dacb9222
Joshua Kinkead <jkinkead@shopkeep.com>
2021-02-26 13:01:53 +0000

    Started some mobile design content and added in fonts for open sans.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'typeface-source-sans-pro';
 import 'typeface-open-sans';
 import Home from './pages/home';
 
