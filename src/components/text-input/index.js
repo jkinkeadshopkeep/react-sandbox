ce428837e23da46dafd3c53ed7e54e170b2bf340
Joshua Kinkead <jkinkead@shopkeep.com>
2021-03-15 11:01:22 +0000

    Error class style added.

--- src/components/text-input/index.js
+++ src/components/text-input/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TextWrapper from "./inputWrapper";
-import './stylesheets/index.module.scss';
+import styles from './stylesheets/index.module.scss';
 
 const TextInput = ({type, name, label, error, ...rest}) => {
     return (
         <TextWrapper label={label} id={name}>
             <input type={type} name={name} {...rest}/>
-            {error[name] && <p className="error">{error[name]}</p>}
+            {error[name] && <p className={styles['error']}>{error[name]}</p>}
         </TextWrapper>
     )
 };
